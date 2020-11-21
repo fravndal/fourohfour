f950ec1b3050aa5a92842092bb24218791356134
kAN <f>
2020-11-21 16:33:42 +0100

    Fixed some footer issues

--- frontend/src/pages/FrontPage.js
+++ frontend/src/pages/FrontPage.js
@@ -9,10 +9,10 @@ const Frontpage = props => {
 
             <Container id="container">
                 <Row className="m-1">
-                    <Col className="test" md="6">
+                    <Col md="6">
                         <Image id="img" src="images/frontpage_mirage.png" fluid="true"></Image>
                     </Col>
-                    <Col className="test2 d-flex align-items-center text-center" md="6" fluid="true">
+                    <Col className="d-flex align-items-center text-center" md="6" fluid="true">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Integer ut auctor nisl. Sed accumsan placerat dignissim. 
                     Quisque iaculis sodales lacus vel accumsan. Curabitur blandit id sem consequat eleifend. 
@@ -21,13 +21,13 @@ const Frontpage = props => {
                     Donec a venenatis lorem.</Col>
                 </Row>
                 <Row className="m-1">
-                    <Col className="test3 d-flex align-items-center text-center" md="6">
+                    <Col className="d-flex align-items-center text-center" md="6">
                     Nulla lobortis eget ligula et lobortis. Integer quis odio nec dui fringilla euismod. 
                     Fusce vitae justo est. Donec porttitor sem mauris, quis luctus dui dignissim vitae. 
                     Nam lacinia tristique sapien vitae dictum. Suspendisse tincidunt commodo nibh, 
                     gravida facilisis nunc maximus ac. Maecenas lacinia ex et libero luctus imperdiet.
                     </Col>
-                    <Col className="test4 d-flex align-items-center text-center" md="6">
+                    <Col className="d-flex align-items-center text-center" md="6">
                         <Image id="img" src="images/frontpage_inferno.png" fluid="true"></Image>
                     </Col>
                 </Row>
