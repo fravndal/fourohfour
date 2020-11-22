f538af1f8973465acfce1b707959ea14cc122d50
Fredrik Ravndal <fredrik.ravndal@allegro.no>
2020-11-22 19:56:51 +0100

    create model for maps

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class UserModel(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+class MapModel(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
--- backend/app/schema.py
+++ backend/app/schema.py
@@ -1,18 +1,29 @@
 from graphene_django import DjangoObjectType
 import graphene
-from app.models import UserModel
+from app.models import UserModel, MapModel
+
 
 
 class UserType(DjangoObjectType):
     class Meta:
         model = UserModel
+        fields = ("id", "name", "last_name")
+
+class Maps(DjangoObjectType):
+    class Meta:
+        model = MapModel
+        fields = ("id", "name", "image")
 
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    maps = graphene.List(Maps)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
+
+    def resolve_maps(self, info):
+        return MapModel.objects.all()
 
 
 class CreateUser(graphene.Mutation):
@@ -34,9 +45,29 @@ class CreateUser(graphene.Mutation):
             last_name=user.last_name,
         )
 
+class CreateMap(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    image = graphene.String()
+
+    class Arguments:
+        name = graphene.String()
+        image = graphene.String()
+
+    def mutate(self, info, name, image):
+        map = MapModel(name=name, image=image)
+        map.save()
+
+        return CreateMap(
+            id=map.id,
+            name=map.name,
+            image=map.image,
+        )
+
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_map = CreateMap.Field()
 
 
 schema = graphene.Schema(
